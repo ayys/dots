2170eb4d8f4f9882979fe456a12a1ee2c26f7235
Ayush Jha <ayush@wasmer.io>
2024-05-12 01:16:50 +0545

    use cyperpunk theme at night

--- emacs.d/packages/themes.el
+++ emacs.d/packages/themes.el
@@ -8,7 +8,7 @@
   (setq calendar-latitude 27.71)
   (setq calendar-longitude 85.32)
   (setq circadian-themes '((:sunrise . tango)
-                           (:sunset . doom-xcode)))
+                           (:sunset . cyberpunk)))
   :config (circadian-setup))
 
 (use-package nyx-theme :ensure )
