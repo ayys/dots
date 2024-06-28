3f5ecb553b0cfc1b35e3495a989f4cd35b1b2d0d
Ayush Jha <ayushjha@proton.me>
2024-06-28 12:09:39 +0545

    add key-chord to emacs

--- emacs.d/packages/utilities.el
+++ emacs.d/packages/utilities.el
@@ -412,3 +412,18 @@ parses its input."
 (use-package ace-window
   :ensure t
   :bind (("M-o" . ace-window)))
+
+
+ (use-package key-chord :ensure t
+        :config
+        (key-chord-mode 1)
+        (key-chord-define-global " j"     'avy-goto-char-timer)
+        (key-chord-define-global " B"     'ibuffer)
+        (key-chord-define-global " b"     'consult-buffer)
+        (key-chord-define-global " p"     'projectile-switch-project)
+        (key-chord-define-global " f"     'projectile-find-file)
+        (key-chord-define-global " r"     'consult-ripgrep)
+        (key-chord-define-global " g"     'magit-status)
+        (key-chord-define-global " c"     'projectile-run-async-shell-command-in-root)
+        (key-chord-define-global " o"     'ace-window)
+        (key-chord-define-global " e"     'consult-outline))
