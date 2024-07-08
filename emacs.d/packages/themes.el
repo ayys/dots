c6581c25d3179349093683af59c980c8620d4f6b
Ayush Jha <ayushjha@proton.me>
2024-07-08 10:31:18 +0545

    change the circadian themes

--- emacs.d/packages/themes.el
+++ emacs.d/packages/themes.el
@@ -6,8 +6,8 @@
   :init
   (setq calendar-latitude 27.71)
   (setq calendar-longitude 85.32)
-  (setq circadian-themes '((:sunrise . solo-jazz)
-                           (:sunset . nyx)))
+  (setq circadian-themes '((:sunrise . ef-deuteranopia-light)
+                           (:sunset . catppuccin)))
   :config (circadian-setup))
 (use-package nyx-theme :ensure )
 
