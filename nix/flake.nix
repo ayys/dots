0cb67034c1a3677e15bd0386c37aeddafa2cd8b1
Ayush Jha <ayushjha@proton.me>
2024-08-10 23:42:56 +0545

    move all nix config to nix/ dir

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,7 +4,7 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
+      url = "github:NixOS/nixpkgs/nixos-24.05";
     };
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -15,15 +15,12 @@
       ayys = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ../configuration.nix
+          ./configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.ayys = import ../home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
+            home-manager.users.ayys = import ./home.nix;
           }
         ];
       };
